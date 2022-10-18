08dfd1b470468d4cd5517154a512491d731851e3
bengisukayan <bengisukayan@gmail.com>
2022-10-18 22:01:08 +0300

    a

--- builtin/environment.c
+++ builtin/environment.c
@@ -6,7 +6,7 @@
 /*   By: bkayan <bkayan@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/18 14:59:58 by bkayan            #+#    #+#             */
-/*   Updated: 2022/10/18 21:41:38 by bkayan           ###   ########.fr       */
+/*   Updated: 2022/10/18 22:00:56 by bkayan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,27 +19,50 @@ void	my_env(t_prompt *p)
 		printf("%s\n", p->envp[i]);
 }
 
+char	*find_key_word(char *a)
+{
+
+}
+
+int	is_present(t_prompt *p, char *a)
+{
+	int	i;
+
+	i = 0;
+	p->envp
+	return (0);
+}
 int	my_unset(t_prompt *p, t_mini *a)
 {
 	int		i;
 	int		j;
+	int		k;
 	char	**temp;
 
-	while (p->envp[i])
-		i++;
-	temp = ft_calloc(i - 1, sizeof(char *));
-	if (!temp)
-		return (-1);
-	i = 0;
-	j = 0;
-	while (p->envp[i])
+	if (a->full_cmd[1])
 	{
-		if (p->envp[i] != a->full_cmd[1])
-			temp[j++] == p->envp[i];
-		i++;
+		k = 1;
+		while (a->full_cmd[k])
+		{
+			while (p->envp[i])
+				i++;
+			temp = ft_calloc(i - 1, sizeof(char *));
+			if (!temp)
+				return (-1);
+			i = 0;
+			j = 0;
+			while (p->envp[i] && is_present(a->full_cmd[k]))
+			{
+				if (p->envp[i] != a->full_cmd[k++])
+					temp[j++] == p->envp[i];
+				i++;
+			}
+			free (p->envp[i]);
+			p->envp = temp;
+		}
+		return (0);
 	}
-	free (p->envp[i]);
-	p->envp = temp;
+	printf("unset: not enough arguments");
 	return (0);
 }
 
