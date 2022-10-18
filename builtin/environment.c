eb01cf088312f4bec6d1e17bbe29aa16ea9aa3b3
bengisukayan <bengisukayan@gmail.com>
2022-10-18 21:43:07 +0300

    env

--- builtin/environment.c
+++ builtin/environment.c
@@ -6,7 +6,7 @@
 /*   By: bkayan <bkayan@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/18 14:59:58 by bkayan            #+#    #+#             */
-/*   Updated: 2022/10/18 20:53:39 by bkayan           ###   ########.fr       */
+/*   Updated: 2022/10/18 21:41:38 by bkayan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,30 @@ void	my_env(t_prompt *p)
 	i = 0;
 	while (p->envp[i])
 		printf("%s\n", p->envp[i]);
+}
+
+int	my_unset(t_prompt *p, t_mini *a)
+{
+	int		i;
+	int		j;
+	char	**temp;
+
+	while (p->envp[i])
+		i++;
+	temp = ft_calloc(i - 1, sizeof(char *));
+	if (!temp)
+		return (-1);
+	i = 0;
+	j = 0;
+	while (p->envp[i])
+	{
+		if (p->envp[i] != a->full_cmd[1])
+			temp[j++] == p->envp[i];
+		i++;
+	}
+	free (p->envp[i]);
+	p->envp = temp;
+	return (0);
 }
 
 void	my_export(t_prompt *p, int ac, t_mini *a)
@@ -36,9 +60,4 @@ void	my_export(t_prompt *p, int ac, t_mini *a)
 			i++;
 		}
 	}
-}
-
-void	my_unset(t_prompt *p)
-{
-	
 }
