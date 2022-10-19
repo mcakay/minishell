0325fde80b5cea08c8195065b606657288f18b26
bengisukayan <bengisukayan@gmail.com>
2022-10-19 03:13:33 +0300

    a

--- builtin/environment.c
+++ builtin/environment.c
@@ -6,7 +6,7 @@
 /*   By: bkayan <bkayan@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/18 14:59:58 by bkayan            #+#    #+#             */
-/*   Updated: 2022/10/19 01:26:51 by bkayan           ###   ########.fr       */
+/*   Updated: 2022/10/19 03:13:19 by bkayan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,7 @@ void	my_env(char **e)
 		printf("%s\n", e[i]);
 }
 
+//takes before =
 char	*find_key_word(char *a)
 {
 	int	i;
@@ -30,15 +31,16 @@ char	*find_key_word(char *a)
 	return (a);
 }
 
-int	is_present(t_prompt *p, char *a)
+//keyword listede var mı
+int	is_present(char **p, char *a)
 {
 	int	i;
 
 	i = 0;
-	while (p->envp_ex[i])
+	while (p[i])
 	{
-		if (find_key_word(p->envp[i]) == find_key_word(a))
-			return (ft_strlen(find_key_word(p->envp_ex[i])));
+		if (find_key_word(p[i]) == find_key_word(a))
+			return (ft_strlen(find_key_word(p[i])));
 		i++;
 	}
 	return (0);
@@ -47,39 +49,53 @@ int	is_present(t_prompt *p, char *a)
 int	my_unset(t_prompt *p, t_mini *a)
 {
 	int		i;
-	int		j;
-	int		k;
 	char	**temp;
 
-	if (a->full_cmd[1])
+	i = 1;
+	while (a->full_cmd[i])
 	{
-		k = 1;
-		while (a->full_cmd[k])
+		if (is_present(p->env, a->full_cmd[i]))
 		{
-			if (is_present(p, a->full_cmd[k]))
-			{
-				while (p->envp[i])
-					i++;
-				temp = ft_calloc(i - 1, sizeof(char *));
-				if (!temp)
-					return (-1);
-				i = 0;
-				j = 0;
-				while (p->envp[i] && is_present(p, a->full_cmd[k]))
-				{
-					if (p->envp[i] != a->full_cmd[k])
-						temp[j++] == p->envp[i];
-					i++;
-				}
-				free (p->envp[i]);
-				p->envp = temp;
-			}
-			k++;
+			temp = change_env(p->env, a->full_cmd[i]);
+			if (!temp)
+				return (-1);
+			p->env = temp;
+			free (temp);
 		}
+		if (is_present(p->env_ex, a->full_cmd[i]))
+		{
+			temp = change_env(p->env_ex, a->full_cmd[i]);
+			if (!temp)
+				return (-1);
+			p->env_ex = temp;
+			free (temp);
+		}
+		i++;
 	}
-	else
-		printf("unset: not enough arguments");
 	return (0);
+}
+
+char	**change_env(t_prompt *p, char **p, char *a)
+{
+	char	**temp;
+	int		i;
+	int		j;
+
+	i = 0;
+	while (p[i])
+		i++;
+	temp = ft_calloc(i - 1, sizeof(char *));
+	if (!temp)
+		return (0);
+	i = 0;
+	j = 0;
+	while (p[i])
+	{
+		if (find_key_word(p[i]) != a)
+			temp[j++] = p[i];
+		i++;
+	}
+	return (temp);
 }
 
 void	my_export(t_prompt *p, int ac, t_mini *a)
